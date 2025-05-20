3236012a05701170835ad6325e2ed57f191bb051
Rohan801651 <rohaanali82005@gmail.com>
2025-05-20 21:52:12 +0500

    Jobs Model added

---
+++ Models/Jobs.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hiring_and_Selection_Process_Platform.Models
+{
+    public class Jobs
+    {
+        [Key]
+        private int jobId { get; set; }
+
+
+        public string jobTitle { get; set; }
+        public string jobDescription { get; set; }
+        public string jobLocation { get; set; }
+        public string jobType { get; set; }  // (part time / full time / intern)
+        public string jobSalary { get; set; }
+        public string jobCompanyName { get; set; }
+        public string jobRequirements { get; set; }
+        public DateTime deadline { get; set; }
+        public int noEmployees { get; set; } // number of employees required for the job
+
+
+
+    }
+}
